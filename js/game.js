ad90fdd4353d0e4e5b2e672f692082449fb68465
AliyaDos <221589@astanait.edu.kz>
2023-05-30 17:51:22 +0600

    MyProject3

--- js/game.js
+++ js/game.js
@@ -55,15 +55,44 @@ const showWinPopup = function() {
     const imageContainer = document.createElement('div');
     imageContainer.classList.add('image-container');
     
-    // You can replace 'path/to/your/image.jpg' with the actual path to your image
     const image = document.createElement('img');
     image.src = 'img/off.jpeg';
     
     imageContainer.appendChild(image);
     popup.appendChild(message);
     popup.appendChild(imageContainer);
+
+    const countdown = document.createElement('div');
+    countdown.id = 'demo';
+    popup.appendChild(countdown);
     
     document.body.appendChild(popup);
+
+    var countDownDate = new Date("June 10, 2023 24:00:00").getTime();
+
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+    // Get today's date and time
+    var now = new Date().getTime();
+    
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+    
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    // Output the result in the countdown element
+    countdown.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+    
+    // If the count down is over, display "EXPIRED"
+    if (distance < 0) {
+      clearInterval(x);
+      countdown.innerHTML = "EXPIRED";
+    }
+  }, 1000);
   }
 
 const showLoosePopup = function() {
